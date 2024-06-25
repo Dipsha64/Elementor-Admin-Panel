a46837ce2a5f60486a52dabf4395ba36eca8fcfa
Dipsha Kalariya <dipshaj64@gmail.com>
2024-06-26 00:09:49 +0530

    Added loading & fildter changes

--- backend_admin/controller/clientControllers/Icons/IconListingController.js
+++ backend_admin/controller/clientControllers/Icons/IconListingController.js
@@ -4,23 +4,19 @@ const fs = require('fs');
 
 const getPaginationIcon = async (req,res) => {
     try{
-        console.log("getPaginationIcon", req.body);
         const page = parseInt(req.body.page) || 1;
         const limit = parseInt(req.body.limit) || 10;
 
         let iconData = null;
         const search = req.body.query;
         const regex = new RegExp(search, 'i');
-        console.log("search..",search);
         const query = search !== '' ? { tag : { $in: [regex] } } : {};
         // let query = {};
         // if (search) {
         //     query = { tags: { $regex: new RegExp(search, 'i') } };
         // }
 
-        console.log("query...",query);
         iconData = await iconItemsModel.find(query).skip((page-1)*limit).limit(parseInt(limit));
-        console.log("API iconData" , iconData.length);
         if(iconData && iconData.length > 0){
             const iconWithImages = iconData.map(item => {
                 const imagePath = path.join(__dirname, '../../../uploads/icons/', item.iconPathName);
